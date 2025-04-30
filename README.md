1322521d25b3403cef8671180ee68eedda268ac9
Cheese <reddgabrinao@gmail.com>
2025-04-30 20:30:49 +0800

    did the intro now :3

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # reddgab.github.io
+A portfolio for my finals. Mostly original code! Access it here: https://reddgab.github.io
--- script.js
+++ script.js
@@ -1,29 +1,47 @@
 
 // ==========toggle navbar===============
 //get items
-const navBar = document.getElementById("nav")
+const navBar = document.getElementById("nav");
       toggleButton = document.getElementById("toggle");
 
 if (navBar) {
-    toggleButton.addEventListener("click", toggleNav)
+    toggleButton.addEventListener("click!", toggleNav);
 }
 
 function toggleNav () {
     if (navBar.classList.contains("hidden")) {
-        toggleButton.classList.add("active")
-        navBar.classList.remove("hidden")
+        toggleButton.classList.add("active");
+        navBar.classList.remove("hidden");
     } else {
-        navBar.classList.add("hidden")
-        toggleButton.classList.remove("active")
+        navBar.classList.add("hidden");
+        toggleButton.classList.remove("active");
     }
 }
 
 // hide navbar when on mobile
 
 function autoHide() {
-    console.log("well the  command ran")
     if (screen.availWidth < 500) {
-        navBar.classList.add("hidden")
-        console.log("it worked")    
+        navBar.classList.add("hidden");
     }
+}
+
+// animate the hello!
+
+const hello = document.getElementById("hello");
+const helloList = ["Hello!", "!لسلام عليكم", "Hallo!", "Bonjour!", "Guten tag!", "Γειά σας!", "שָׁלוֹם!", "नमस्ते!", "Dia duit!", "Salve!", "こんにちは!", "안녕하세요!", "你好!", "สวัสดีครับ!", "Olá!", "Toki!", "Hola!", "Hej!", "Kamusta!", " !مرحبا"];
+
+function multilanguage () {
+    let i = 0;
+    function changeHello () {
+        if (i >= helloList.length) {
+            i=0
+        }
+        hello.innerHTML = helloList[i];
+        
+        i++;
+    }
+
+    setInterval(changeHello, 3000);
+    
 }
