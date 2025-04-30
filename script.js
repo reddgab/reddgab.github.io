4cec55809afd6b6a23a491ef53c476f4527c3268
reddgab <reddgabrinao@gmail.com>
2025-04-29 23:13:24 -0400

    fixed the navbar for mobile
    and also made the gradient linear

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// ==========toggle navbar===============
 //get items
 const navBar = document.getElementById("nav")
       toggleButton = document.getElementById("toggle");
@@ -7,10 +9,21 @@ if (navBar) {
 }
 
 function toggleNav () {
-    console.log("i got clicked")
     if (navBar.classList.contains("hidden")) {
+        toggleButton.classList.add("active")
         navBar.classList.remove("hidden")
     } else {
         navBar.classList.add("hidden")
+        toggleButton.classList.remove("active")
+    }
+}
+
+// hide navbar when on mobile
+
+function autoHide() {
+    console.log("well the  command ran")
+    if (screen.availWidth < 500) {
+        navBar.classList.add("hidden")
+        console.log("it worked")    
     }
 }
